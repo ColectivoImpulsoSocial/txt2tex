02785de2a4372b626a8de43145e63c86e1e9a2ff
edgardosgtsub <edgardosgtsub@gmail.com>
2016-02-23 11:18:14 -0600

    Versión 2.0
    Actualizando el algoritmo para ir mostrando partes del texto de tal forma que ahora ya se puede cambiar palabras, conjunto de palabras.
    
    Ejemplo: Cuando salga la solicitud de opcion usar:
       i 7 hola  -> Esto reemplazará el caracter 7 por hola
       i 5,19 cambiando dos -> Esto reemplazará lo que esté en la palabra 5 y 19 por cambiando y dos respectivamente
       i 5-10 cambiando un conjunto de palabras -> Esto reemplazará de los caracteres 5 al 10 por "cambiando un conjunto de palabras". Esto borrará los saltos de línea por ahora.
    
    Aún no se cambia todo el documento una vez usado un cambio, pero en la siguiente versión lo hará.

--- txttotex.pl
+++ txttotex.pl
@@ -13,7 +13,11 @@ my $fo;
 my $salto_pagina;
 my $lineas_por_pagina;
 my @texto_completo;
+my @texto_en_palabras;
 my $texto_size;
+my $texto_nueva_linea = 20;
+my $max_heigth = 3;
+my $max_width = 100;
 ####### Read input arguments   #####
 my $opt_help = undef;
 my $texts_dir = undef;
@@ -36,14 +40,15 @@ GetOptions('help'=> \$opt_help,
 &process_args();
 &merge_pages();
 &initialize_latex();
-&correction() if (!defined $no_correction);
+#&correction() if (!defined $no_correction);
+&corregir();
 &close_latex();
 
 ###################################################################
 sub process_args(){
 #Ayuda
 	&print_help() if (defined $opt_help);
-#Si no hay carpeta qué procesar, salir con error.
+#Si no hay carpeta quÃ© procesar, salir con error.
 	die("Se necesita una carpeta con imagenes para usar el programa o un archivo de texto con lo convertido.") if(!defined $texts_dir && !defined $input_text);
 #Asignar la carpeta de salida en base a la de entrada si no se define
 	#($output_dir = $texts_dir) =~ s/(.*)/$1_txttotex/ if (!defined $output_dir);
@@ -51,117 +56,193 @@ sub process_args(){
 	$tex_name .= ".tex";
 }
 sub merge_pages(){
+   my $linea;
    `cat *.txt > mergedfile.temp` if(defined $texts_dir);
 
    open ($fi, "< :encoding(Latin1)", $input_text) or die "No se pudo abrir archivo: $input_text";
    open ($fo, '>', $tex_name) or die "No se pudo abrir archivo: $tex_name";
-   while (my $linea = <$fi>){
-      push @texto_completo, $linea;
+   while ($linea = <$fi>){
+      push @texto_completo, $linea if ($linea !~ /^\s*$/);
    }
-   $texto_size = @texto_completo;
+   foreach $linea (@texto_completo){
+      $linea =~ s/\n//g;
+      print "new_line found: $linea" if($linea =~ /\n/);
+      $linea .= " <NUEVA_LINEA>" if ($texto_nueva_linea >= length $linea);
+#chomp $linea;
+      #if($linea !~ /Ë†\s*$/){
+         push @texto_en_palabras, split / /, $linea;
+      #}
+   }
+   $texto_size = @texto_en_palabras;
+   #foreach my $palabra (@texto_en_palabras){
+   #   print "--$palabra--";
+   #}
 }
 sub initialize_latex(){
    print $fo "\\documentclass[12pt]{article}\n";
    print $fo "\\begin{document}\n";
    #&set_new_page();
 }
-sub correction(){
-   my $n_l = 10;
-   my $max_h = 100;
-   my $new_line = 30;
-   my $area = $max_h*$n_l;
-   my $opcion;
-#my $new_line = 
-   my $i=0; my $palabras_length; my $temp;
-   my @palabras; my $item; my $mayor=0;
-   my @palabras_total; my @next_palabras2; my @palabras2;
-   my @numeros; my @next_numeros; my $num_item;
-   my $area_acumulada=0; my $next_area_acumulada=0;
-   my $linea_area=0; my $new_line_flag = 0;
-   my $ordd; my $sum_ordd; my $odd_flag=0;
-   my $palabra_final; my $real_length; my $numero_final;
-   my $area_a2; my @palabras_copy; my @numeros_copy; my $j; my $numero;
-   my $count; my $texto_count;
-   foreach my $linea (@texto_completo){
-      $texto_count++;
-#print "$linea\n";
-      @palabras = split /\s+/,$linea;
-      foreach $item (@palabras){
-         $i++;
-         ($palabra_final,$real_length,$numero_final) =  &rellenar_espacios($item,$i);
-         $area_acumulada += $real_length+1;
-         $linea_area += $real_length+1;
-         #print "---$item---\n---$temp---\n Char by char: ";
-         push @numeros, $numero_final;
-         push @palabras_total, $palabra_final;
-#print "area: $area_acumulada, i: $i, palabra: $item, numero: ---$temp---\n";
-      }
-      if (($linea_area < $new_line && $linea_area > 0) || ($linea_area eq 0 && $new_line_flag eq 0)){
-         #print "detectada nueva linea, linea_area=$linea_area, area_acumulada antes: $area_acumulada\n";
-         push @numeros, $max_h-$linea_area if($linea_area>0);
-         push @numeros, 0 if($linea_area<=0);
-         push @palabras_total, "TERMINAR";
-         $area_acumulada += $max_h-$linea_area if($linea_area>0);
-         #print "detectada nueva linea, area_acumulada despues: $area_acumulada\n";
-         $new_line_flag=1;
-         $linea_area = 0;
-      }else{
-         $new_line_flag=0;
-      }
-      $linea_area = 0;
-      if ($area_acumulada>=$area){
-         #print "$area_acumulada, $area\n";
-         $area_a2 = $area_acumulada;
-         @palabras_copy = @palabras_total;
-         @numeros_copy  = @numeros;
-         $j=0;
-         while($area_a2>$area){
-            $item = pop @palabras_copy;
-            $numero = pop @numeros_copy;
-            if ($item ne "TERMINAR"){
-               $j++;
-               $item =~ s/\s//g;
-               ($palabra_final,$real_length,$numero_final,) = &rellenar_espacios($item,$j);
-               $area_a2-= $real_length+1;
-            }else{
-               $area_a2-= $numero;
-            }
-         }
-         $i=$j;
-         while($area_acumulada>$area){
-            $item = pop @palabras_total;
-            $numero = pop @numeros;
-            if ($item ne "TERMINAR"){
-               $item =~ s/\s//g;
-               ($palabra_final,$real_length,$numero_final,) = &rellenar_espacios($item,$j);
-               $area_acumulada-=$real_length+1;
-               $next_area_acumulada+=$real_length+1;
-               unshift @next_numeros, $numero_final;
-               unshift @next_palabras2, $palabra_final;
-               $j--;
-               #print "Eliminado: palabra: $item, numero: $numero_final, area: $area_acumulada\n";
-            }else{
-               $area_acumulada-=$numero;
-               $next_area_acumulada+=$numero;
-               #print "TERMINAR FOUND, not adding!\n";
-            }
-         }
-         &imprimir_tabla(\@palabras_total,\@numeros,$max_h);
-         @palabras_total = @next_palabras2;
-         $area_acumulada = $next_area_acumulada;
-         $linea_area = $area_acumulada;
-         @numeros = @next_numeros;
-         @next_palabras2=();
-         @next_numeros=();
-         $next_area_acumulada=0;
-         #print "Nueva area acumulada: $area_acumulada, area de linea: $linea_area, i final=$i\n";
-         print "Introduzca una opcion (1) o (2):\t";
-         $opcion = <>;
-         print "\n";
-         #print "opcion escogida: $opcion\n contador: $texto_count, texto_siguiente: $texto_completo[$texto_count]\n";
-      }
 
+sub corregir(){
+   my $word_index = 0;
+   my $word_beginning = 0;
+   my $height_count = 1;
+   my $width_count = 0;
+   my $word_length;
+   my $print_index;
+   my $index = 0;
+   my $real_length;
+   my $word;
+   while ($word_index < $texto_size){
+      $word = $texto_en_palabras[$word_index];
+      #print $word;
+      if ($word eq "<NUEVA_LINEA>"){
+         $word_index++;
+         $width_count = 0;
+         $height_count++;
+         $width_count = $max_heigth+1 if ($height_count > $max_heigth);
+         #print "found new line, new width_count: $width_count";
+      }else{
+         $index++;
+         ($word,$real_length,$print_index) = &rellenar_espacios($word,$index);
+         if ($width_count + $real_length +1> $max_width || ($word_index +1) eq $texto_size){
+            #print "SI. $width_count+$real_length +1 > $max_width, $word,$word_index-1,$texto_size,$height_count,$max_heigth\n";
+            if ($height_count + 1 > $max_heigth || ($word_index +1)eq $texto_size){
+               $word_index++ if($word_index +1 eq $texto_size);
+               &seleccion($word_beginning,$word_index-1);
+               $height_count = 1;
+               $width_count  = 0;
+               $index = 0;
+               $word_beginning = $word_index;
+            }else{
+               $word_index++;
+               $width_count = $real_length;
+               $height_count++;
+            }
+         }else{
+            #print "NO: $width_count+$real_length +1 > $max_width, $word,$word_index-1,$height_count,$max_heigth\n";
+            $width_count += $real_length +1;
+            $word_index++;
+         }
+      }
    }
+}
+sub seleccion(){
+   my $low_index  = $_[0];
+   my $high_index = $_[1];
+   my $opcion;
+   &print_table($low_index,$high_index);
+   print "Introduzca una opcion (1) o (2):\t";
+   $opcion = <>;
+   $opcion =~ s/\n//;
+   print "opcion escogida: <$opcion>\n";
+   ($low_index,$high_index) = &modificar($opcion,$low_index,$high_index);
+   &seleccion($low_index,$high_index) if($opcion ne 0);
+
+}
+sub print_table(){
+   my $low_index  = $_[0];
+   my $high_index = $_[1];
+   my $index = 1;
+   my $palabra; my $real_length; my $print_index;
+   my @numeros;
+   my $width_count = 0;
+   my $numero;
+   while ($low_index <= $high_index){
+      if ($texto_en_palabras[$low_index] ne "<NUEVA_LINEA>"){
+         $palabra = $texto_en_palabras[$low_index];
+         ($palabra,$real_length,$print_index) = &rellenar_espacios($palabra,$index++);
+         if ($width_count + $real_length +1 > $max_width){
+            print "\n";
+            foreach $numero (@numeros){
+               print RED, "$numero ", RESET;
+            }
+            print "\n\n";
+            $width_count = 0;
+            @numeros=($print_index);
+         }else{
+            push @numeros, $print_index;
+         }
+         print GREEN, "$palabra ", RESET;
+         $width_count += $real_length +1;
+      }else{
+         print "\n";
+         foreach $numero (@numeros){
+            print RED, "$numero ", RESET;
+         }
+         print "\n\n";
+         $width_count = 0;
+         @numeros=();
+      }
+      $low_index++;
+   }
+   print "\n";
+   foreach $numero (@numeros){
+      print RED, "$numero ", RESET;
+   }
+   print "\n\n";
+}
+sub modificar(){
+   my $comando    = $_[0];
+   my $low_index  = $_[1];
+   my $high_index = $_[2];
+   my $local_index;
+   my @cm   = split /\s+/, $comando;
+   my @rango; my $fix;
+   my $letra = shift @cm;
+   my $pos; my $cm_length; my $menor; my $mayor; my $tipo;
+   my $low_copy = $low_index;
+   my @new_lines; my $new_line_count=0;
+   while ($low_copy <= $high_index){
+      #print "$texto_en_palabras[$low_copy]";
+      #print "\n";
+      if($texto_en_palabras[$low_copy] eq "<NUEVA_LINEA>"){
+         #print "pushing: $low_copy\n";
+         push @new_lines, $low_copy+$new_line_count;
+         splice @texto_en_palabras, $low_copy, 1;
+         #print "after pushing: $texto_en_palabras[$low_copy]\n";
+         $new_line_count++;
+      }else{
+         $low_copy++;
+      }
+   }
+   if ($letra eq "i"){
+      $tipo = shift @cm;
+      if ($tipo =~ /,/){
+         push @rango, split /,/,$tipo;
+         $local_index=0;
+         foreach $pos (@rango){
+            $texto_en_palabras[$low_index+$pos-1] = $cm[$local_index++];
+         }
+      }elsif($tipo =~ /-/){
+         ($menor,$mayor) = split /-/,$tipo;
+         $cm_length = @cm;
+         splice @texto_en_palabras, $low_index+$menor-1, $mayor-$menor+1, @cm;
+         $fix =  $cm_length - ($mayor - $menor + 1);
+         $texto_size += $fix;
+         $high_index += $fix;
+      }else{
+         $texto_en_palabras[$low_index+$tipo-1] = $cm[0];
+         #print "$low_index,$tipo,$cm[0],$texto_en_palabras[$low_index+$tipo]\n";
+      }
+   }
+   if ($tipo =~ /,/ || $tipo !~ /-/){
+      foreach my $new_line (@new_lines){
+         splice @texto_en_palabras, $new_line, 0, "<NUEVA_LINEA>";
+      }
+   }else{
+      foreach my $new_line (@new_lines){
+         if ($new_line <= $menor){
+            splice @texto_en_palabras, $new_line, 0, "<NUEVA_LINEA>";
+            $new_line_count++;
+         }elsif($new_line > $mayor){
+            #print "fix: $fix, menor: $menor, mayor: $mayor\n";
+            splice @texto_en_palabras, $new_line+$fix, 0, "<NUEVA_LINEA>";
+         }
+      }
+   }
+   return ($low_index,$high_index);
 }
 sub rellenar_espacios(){
    my $palabra = $_[0];
@@ -213,54 +294,7 @@ sub log10(){
    $log_10++ while(10**$log_10<$num);
    return $log_10;
 }
-sub imprimir_tabla(){
-   my @palabras = @{$_[0]};
-   my @numeros  = @{$_[1]};
-   my $max_h    = $_[2];
-   my $palabra_length;
-   my $length_acumulada=0;
-   my $linea_area=0;
-   my $count=0; my $count2=0;
-   my $p_t=0; my $i=0; my $i_p;
-   foreach my $palabra (@palabras){
-      $count++;
-      if ($palabra ne "TERMINAR"){
-	 print YELLOW, "$palabra ", RESET;
-	 $palabra_length = length $palabra;
-	 $length_acumulada+=$palabra_length+1;
-	 $linea_area+=$palabra_length+1;
-	 if ($length_acumulada > $max_h){
-	    print "\n";
-	    while ($count2<$count){
-	       print GREEN, "$numeros[$count2] ", RESET;
-	       $count2++;
-	    }
-	    print "\n\n";
-	    $length_acumulada=0;
-	    $linea_area=0;
-	 }
-      }else{
-	 #print "TERMINAR\n";
-	 print "\n";
-	 $palabra_length = $max_h - (length $linea_area);
-	 $length_acumulada+=$palabra_length+1;
-	 while ($count2<$count-1){
-	    print GREEN, "$numeros[$count2] ", RESET;
-	    $count2++;
-	 }
-	 $count2++;
-	 print "\n\n";
-	 $length_acumulada=0;
-	 $linea_area=0;
-      }
-   }
-   print "\n";
-   while ($count2<$count){
-      print GREEN, "$numeros[$count2] ", RESET;
-      $count2++;
-   }
-   print "\n\n";
-}
+
 sub close_latex(){
    print $fo "\\close{document}\n";
 }
@@ -282,7 +316,7 @@ sub print_help(){
     --help              Print this help message and exit.
     --input_dir <path>  El path que contenga las carpetas a convertir.
     --ocrs "t,o,g"      Usar una o varias. Ejmplo: --ocrs t, --ocrs "t,o".
-    --output_dir <path> Path en donde se guardarán las conversiones.
+    --output_dir <path> Path en donde se guardarÃ¡n las conversiones.
     --formats "txt,pdf" Formatos a usar. Uno o varios. Ejemplo: --formats txt.
 
     Por default --ocrs es t (Tesseract), --output_dir es <input_dir>_convertido, --formats es txt.
