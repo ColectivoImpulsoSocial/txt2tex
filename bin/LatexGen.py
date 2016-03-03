730db28649fe0cbbdb78121a657afcc88ab2cc7a
JOAS <solojoas@gmail.com>
2016-03-03 00:59:13 -0600

    Inicia el "porting" de código a python.

---
+++ bin/LatexGen.py
@@ -0,0 +1,6 @@
+import  argparse
+
+parser = argparse.ArgumentParser(description =
+    'Este script convierte una serie de imagenes de texto a archivos txt o pdf usando OCRs libres')
+
+args = parser.parse_args()
---
+++ bin/OcrConverter.py
@@ -0,0 +1,9 @@
+
+
+class OcrInterface:
+    """
+    Clase para ser una interface del 
+    OCR instalado en el SO utilizado
+    """
+    pass
+
